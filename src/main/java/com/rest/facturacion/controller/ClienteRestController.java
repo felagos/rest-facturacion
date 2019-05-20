a1f46c415bffad82490ed65d62a697d5213a9c28
felagos <felagossanchez@gmail.com>
2019-05-20 08:25:11 -0400

    endpoint para crear clientes

--- src/main/java/com/rest/facturacion/controller/ClienteRestController.java
+++ src/main/java/com/rest/facturacion/controller/ClienteRestController.java
@@ -1,21 +1,15 @@
 package com.rest.facturacion.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.rest.facturacion.Response;
 import com.rest.facturacion.entities.Cliente;
 import com.rest.facturacion.services.interfaces.IClientService;
@@ -57,6 +51,11 @@ public class ClienteRestController {
 		
 		return ResponseEntity.ok().body(new Response(HttpStatus.OK, ""));
 
+	}
+	
+	@PostMapping("/guardar")
+	public void guardar(Cliente cliente) {		
+		this.clienteService.save(cliente);
 	}
 
 }
